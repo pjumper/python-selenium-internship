4f944928965c5fdd214d5fdde9f81d2ff7c2a07c
User <user@users-mbp.attlocal.net>
2023-05-07 20:03:47 -0700

    Task 6

--- features/environment.py
+++ features/environment.py
@@ -5,8 +5,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.service import service
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
-def browser_init(context, test_name):
+def browser_init(context, scenario):
     """
     :param context: Behave context
     :param test_name: scenario.name
@@ -25,31 +27,45 @@ def browser_init(context, test_name):
     #options.headless = True
     #context.driver = webdriver.Firefox(executable_path='geckodriver', options=options)
     #######################################################
-    #context.driver = webdriver.Chrome('service=service')
+    context.driver = webdriver.Chrome('service=service')
     #context.driver = webdriver.Firefox(service=service)
 
     #context.driver = webdriver.Safari()
     #context.driver = webdriver.Firefox()
 
     ##HEADLESS MODE ##
-    # options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')
-    # context.driver = webdriver.Chrome(
-    #     chrome_options=options,
-    #     service=service
-    # )
+    #options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
+    #context.driver = webdriver.Chrome(
+        #chrome_options=options,
+        #service=service
+    #)
 
     ### for browserstack ###
-    desired_cap = {
-        'browser': 'Chrome',
-        'os_version': '11',
-        'os': 'Windows',
-        'name': test_name
-    }
-    bs_user = 'chriscrayton_YJMgRK'
-    bs_key = 'g3UKWUehpdP2scReX7WL'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+    #desired_cap = {
+        #'browser': 'Chrome',
+        #'os_version': '11',
+        #'os': 'Windows',
+        #'name': test_name
+    #}
+    #bs_user = 'chriscrayton_YJMgRK'
+    #bs_key = 'g3UKWUehpdP2scReX7WL'
+    #url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+
+    #context.driver = webdriver.Chrome(executable_path='chromedriver', desired_capabilities=capabilities)
+    #mobile_emulation = {"deviceName": "Pixel 5"}
+    #chrome_options = webdriver.ChromeOptions()
+    #chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    #driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=chrome_options.to_capabilities())
+
+    mobile_emulation = {
+        "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
+        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
+    chrome_options = Options()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(5)

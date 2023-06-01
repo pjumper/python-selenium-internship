c20b67f29f7514d2d0973bf694322013103cd7ad
Chris <user@mac-mini.attlocal.net>
2023-05-31 19:41:16 -0700

    Added Test Case 'Color Options'

--- features/environment.py
+++ features/environment.py
@@ -54,18 +54,12 @@ def browser_init(context, scenario):
     #context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
     #context.driver = webdriver.Chrome(executable_path='chromedriver', desired_capabilities=capabilities)
-    #mobile_emulation = {"deviceName": "Pixel 5"}
+    #mobile_emulation = {"deviceName": "iPhone 12 Pro"}
     #chrome_options = webdriver.ChromeOptions()
     #chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-    #driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=chrome_options.to_capabilities())
+    #context.driver = webdriver.Chrome(options=chrome_options)
 
-    ###Mobile Emulation###
-    mobile_emulation = {
-        "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
-        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
-    chrome_options = Options()
-    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+
 
 
     context.driver.maximize_window()
